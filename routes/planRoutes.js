40c7d9acf854f743676cddb50416b42889e06ac5
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-08-29 10:43:27 +0530

    commit

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -10,11 +10,10 @@ const {
 } = require("../controllers/planController");
 
 const router = express.Router();
-router
-  .post("/create-plan", auth, isAdmin, createPlan)
-  .get("/get-plans", getAllPlans)
-  .get("/get-plan/:id", getPlan)
-  .patch("/update-plan/:id", auth, isAdmin, updatePlan);
+router.post("/create-plan", auth, isAdmin, createPlan);
+router.get("/get-plans", getAllPlans);
+router.get("/get-plan/:id", getPlan);
+router.patch("/update-plan/:id", auth, isAdmin, updatePlan);
 router.delete("/delete-plan/:id", auth, isAdmin, deletePlan);
 
 module.exports = router;
