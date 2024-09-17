51335a2829bae3c98ba0f391a7c90e65c7d58946
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-09-17 11:40:05 +0530

    commit

--- utils/cronJobs.js
+++ utils/cronJobs.js
@@ -50,6 +50,23 @@ cron.schedule("*/30 * * * *", async () => {
         await transactionModel.insertMany(transaction);
       }
     }
+
+    const transporter = nodeMailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: "Keep It Going <keepitgoingstory@gmail.com>",
+      to: "shobhitchoudhary745@gmail.com",
+      subject: "optionssubject",
+      text: "Success",
+    });
   } catch (error) {
     const transporter = nodeMailer.createTransport({
       host: process.env.SMTP_HOST,
