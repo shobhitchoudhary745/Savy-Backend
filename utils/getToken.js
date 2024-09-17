c1d319204e993965841bfb9f90fb1a9aede59175
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-09-17 13:30:57 +0530

    commit

--- utils/getToken.js
+++ utils/getToken.js
@@ -2,22 +2,24 @@ const API_KEY = process.env.BASIC_API;
 const axios = require("axios");
 const getToken = () => {
   return new Promise(async (resolve, reject) => {
-    const config = {
-      method: "post",
-      maxBodyLength: Infinity,
-      url: "https://au-api.basiq.io/token",
-      headers: {
-        Authorization: `Basic ${API_KEY}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-        "basiq-version": "3.0",
-      },
-      data: { scope: "SERVER_ACCESS" },
-    };
     try {
-      const { data } = await axios(config);
+      const { data } = await axios.post(
+        "https://au-api.basiq.io/token",
+        {
+          scope: "SERVER_ACCESS",
+        },
+        {
+          headers: {
+            Authorization: `Basic ${process.env.BASIC_API}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+            "basiq-version": "3.0",
+          },
+        }
+      );
+
       resolve(data.access_token);
-    } catch (err) {
-      reject(err);
+    } catch (error) {
+      reject();
     }
   });
 };
